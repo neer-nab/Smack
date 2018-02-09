fa97699393cad0c0c40f793fb06b44e734db0655
neer-nab <neerajk207@gmail.com>
2018-02-09 17:08:37 +0530

    Login Activity Added

--- app/src/main/java/com/example/neervyas/smack/MainActivity.kt
+++ app/src/main/java/com/example/neervyas/smack/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.neervyas.smack
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
@@ -38,6 +39,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun loginBtnNavClicked(view: View){
+
+        val loginIntent = Intent(this, LoginActivity::class.java)
+        startActivity(loginIntent)
 
     }
 
