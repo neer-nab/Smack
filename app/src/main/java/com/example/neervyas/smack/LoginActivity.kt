5b0cb7672dc0f1755d96e702bd99c855f5d001c9
neer-nab <neerajk207@gmail.com>
2018-02-09 17:29:12 +0530

    Create User Activity Added

--- app/src/main/java/com/example/neervyas/smack/LoginActivity.kt
+++ app/src/main/java/com/example/neervyas/smack/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.neervyas.smack
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -12,6 +13,9 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun loginCreateUserBtnClicked (view: View) {
+
+        val createUserIntent = Intent(this, CreateUserActivity::class.java)
+        startActivity(createUserIntent)
 
     }
 
